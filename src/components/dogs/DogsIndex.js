d388a2d7c0befa48d500af584bad40a3ae268f46
Brittany Crocker <brittany.crocker@outlook.com>
2020-12-02 17:48:42 +1100

    Text and Url posting to server

--- src/components/dogs/DogsIndex.js
+++ src/components/dogs/DogsIndex.js
@@ -4,18 +4,24 @@ import axios from 'axios';
 const SERVER_URL = 'http://localhost:5000/api/hello'
 const SERVER_URL_POST = 'http://localhost:5000/api/world'
 
+let urlText = ''
+
 class DogsIndex extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
       response: '',
       post: '',
       responseToPost: '',
-      dogPosts: []
+      dogPosts: [],
+      url: ''
     }
+
+    this.setUrl = this.setUrl.bind(this)
 
     const fetchDogs = async() => {
         await axios.get(SERVER_URL).then((response) => {
+          console.log("FETCH DOGS: ", response.data)
         this.setState({dogPosts: response.data.posts })
         console.log(response.data)
 
@@ -25,9 +31,15 @@ class DogsIndex extends Component {
       fetchDogs()
   }
 
+setUrl(url) {
+  this.setState({url: url})
+  urlText = url
+}
   savePost( content ){
-    console.log("content", {text: content})
-    axios.post(SERVER_URL, {text: content})
+    console.log("content", content, urlText)
+    content = content.toString()
+    console.log("CONTENT: ", content)
+    axios.post(SERVER_URL, ({text: content, url: urlText}))
       .then((result) => {
         console.log("RESULT", result.config.data)
       // this.state.dogPosts.push(result.config.data)
@@ -38,7 +50,7 @@ class DogsIndex extends Component {
     return (
       <div>
         <h1>A quick not from dogs around the world</h1>
-          <DogPostForm onSubmit={this.savePost}/>
+          <DogPostForm onSubmit={this.savePost} onChange={this.setUrl}/>
 
           <p>{this.state.dogPosts}</p>
       </div>
@@ -49,14 +61,24 @@ class DogsIndex extends Component {
 class DogPostForm extends Component {
   constructor() {
     super();
-    this.state = {content: ''};
+    this.state = {
+      content: '',
+      url: ''
+  };
     this._handleChange = this._handleChange.bind(this)
+    this._handleUrlChange = this._handleUrlChange.bind(this)
     this._handleSubmit = this._handleSubmit.bind(this)
   }
 
   _handleChange(event) {
     this.setState({content: event.target.value})
   }
+
+  _handleUrlChange(event) {
+    this.setState({url: event.target.value})
+    this.props.onChange(event.target.value)
+  }
+
 
   _handleSubmit(event) {
     event.preventDefault();
@@ -69,10 +91,18 @@ class DogPostForm extends Component {
     return(
       <form onSubmit={ this._handleSubmit }>
         <textarea onChange={ this._handleChange } value={ this.state.content }/>
+        <textarea onChange={this._handleUrlChange}  value={ this.state.url } />
         <input type="submit" value="Tell" />
       </form>
     );
   }
+}
+
+const PostDisplay = (props) => {
+  return (
+    <div>
+    </div>
+  )
 }
 
 
