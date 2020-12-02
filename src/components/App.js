718040f8bbfd4f653200f2bc3b6888e247202a46
Brittany Crocker <brittany.crocker@outlook.com>
2020-12-02 17:07:43 +1100

    get and post successfully workinging for DogsIndex

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import MainPage from '../components/main/MainPage'
 import DogsIndex from '../components/dogs/DogsIndex'
 
@@ -5,7 +12,14 @@ function App() {
   return (
     <div className="App">
       <MainPage />
-      <DogsIndex />
+      <Router>
+      <Link to="/dogsindex">Dogs around the world</Link>
+      <Switch>
+      <Route path="/dogsindex">
+      <DogsIndex/>
+      </Route>
+      </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/dogs/DogsIndex.js
+++ src/components/dogs/DogsIndex.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const SERVER_URL = 'http://localhost:5000/api/hello'
+const SERVER_URL_POST = 'http://localhost:5000/api/world'
 
 class DogsIndex extends Component {
   constructor(){
@@ -10,29 +11,67 @@ class DogsIndex extends Component {
       response: '',
       post: '',
       responseToPost: '',
+      dogPosts: []
     }
 
     const fetchDogs = async() => {
-        await axios.get(SERVER_URL).then((response) =>
-        this.setState({response: response.data })
+        await axios.get(SERVER_URL).then((response) => {
+        this.setState({dogPosts: response.data.posts })
+        console.log(response.data)
+
+      }
       )}
 
       fetchDogs()
   }
 
-
-
-
-
-
+  savePost( content ){
+    console.log("content", {text: content})
+    axios.post(SERVER_URL, {text: content})
+      .then((result) => {
+        console.log("RESULT", result.config.data)
+      // this.state.dogPosts.push(result.config.data)
+    })
+  }
 
   render () {
     return (
       <div>
         <h1>A quick not from dogs around the world</h1>
+          <DogPostForm onSubmit={this.savePost}/>
 
+          <p>{this.state.dogPosts}</p>
       </div>
     )
+  }
+}
+
+class DogPostForm extends Component {
+  constructor() {
+    super();
+    this.state = {content: ''};
+    this._handleChange = this._handleChange.bind(this)
+    this._handleSubmit = this._handleSubmit.bind(this)
+  }
+
+  _handleChange(event) {
+    this.setState({content: event.target.value})
+  }
+
+  _handleSubmit(event) {
+    event.preventDefault();
+    this.props.onSubmit(this.state.content);
+    this.setState({content: ''})
+
+  }
+
+  render() {
+    return(
+      <form onSubmit={ this._handleSubmit }>
+        <textarea onChange={ this._handleChange } value={ this.state.content }/>
+        <input type="submit" value="Tell" />
+      </form>
+    );
   }
 }
 
