c3c73d4845d6fe10549453fe746564203f34b2bc
Brittany Crocker <brittany.crocker@outlook.com>
2020-12-04 00:13:03 +1100

    added dog image icon to nav

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ import ParkCard from '../components/main/ParkCard'
 import DogsIndex from '../components/dogs/DogsIndex'
 import { Container, Grid } from '@material-ui/core';
 import './App.css';
+import img from "../components/main/img/icons8-dog-64.png"
 
 
 function App(props) {
@@ -19,6 +20,7 @@ function App(props) {
       <HashRouter>
         <nav id="headerStyle" className="navbar navbar-expand-lg navbar-dark bg-success">
           <div className="navbar-nav">
+            <img src={img}/>
             <Link id="green" className="navbar-brand">Happy Tails</Link>
             <Link className="nav-link" to='/'>Dog Park Search</Link>
             <Link className="nav-link" to='/dogsindex'>Notes From Dogs Around The World</Link>
